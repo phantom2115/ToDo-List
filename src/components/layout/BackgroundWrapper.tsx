16329be20e26219037ae59171032ff9450f21a04
정민석 <minskyson7@gmail.com>
2025-08-20 03:43:40 +0900

    refactor: layout 수정

--- src/components/layout/BackgroundWrapper.tsx
+++ src/components/layout/BackgroundWrapper.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { cn } from "@/lib/utils";
 import { usePathname } from "next/navigation";
 
 interface BackgroundWrapperProps {
@@ -14,9 +15,10 @@ export default function BackgroundWrapper({
 
   return (
     <div
-      className={`lg:px-[360px] md:px-6 px-4 pb-10 ${
+      className={cn(
+        "lg:px-[360px] md:px-6 px-4 pb-10",
         isDetailPage && "lg:bg-[#F9FAFB]"
-      }`}
+      )}
     >
       {children}
     </div>
